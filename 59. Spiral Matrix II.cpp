911c7ebd1afa5eb594f26bbcb9bae8ba1acbaf57
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-07-07 22:11:31 +0530

    Update 59. Spiral Matrix II.cpp

--- 59. Spiral Matrix II.cpp
+++ 59. Spiral Matrix II.cpp
@@ -1,1 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
 
+    
+        int right=0;
+        int bottom=0;
+        int left=n-1;
+        int top=n-1;
+        int counter=1;
+        
+        vector<vector<int>> v( n , vector<int> (n));  
+
+        while(top>=bottom && left>=right){
+            for(int i=right;i<=left;i++){
+                v[bottom][i]=counter;
+                counter++;
+            }
+            
+            bottom++;
+            for(int i=bottom;i<=top;i++){
+                v[i][left]=counter;
+                counter++;
+            }
+            
+            left--;
+            if(top>=bottom){
+                for(int i=left;i>=right;i--){
+                    v[top][i]=counter;
+                    counter++;
+                }
+                top--;
+                
+            }
+            
+            if(left>=right){
+                for(int i=top;i>=bottom;i--){
+                    v[i][right]=counter;
+                    counter++;
+                }
+                right++;
+                
+            }
+        }
+        
+        return v;
+    }
+};
