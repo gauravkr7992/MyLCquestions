df6b5b969bf83ce3b809076920e49e27b07d7ee8
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-07-18 18:20:57 +0530

    Update 2259. Remove Digit From Number to Maximize Result.cpp

--- 2259. Remove Digit From Number to Maximize Result.cpp
+++ 2259. Remove Digit From Number to Maximize Result.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    string max1(string a,string b){
-    for(int i = 0; i < a.length(); i++){
-    if ((a[i]-'0') < (b[i]-'0')){
-        return b;
-        break;
-        }
-    else if((a[i]-'0') > (b[i]-'0')) {
-        return a;
-        break;
-    }
-    }
-        return a;
-    }
-    
+    // string max1(string a,string b){
+    // for(int i = 0; i < a.length(); i++){
+    // if ((a[i]-'0') < (b[i]-'0')){
+    //     return b;
+    //     break;
+    //     }
+    // else if((a[i]-'0') > (b[i]-'0')) {
+    //     return a;
+    //     break;
+    // }
+    // }
+    //     return a;
+    // }
+    // inbuilt max(str a,str b) is in C++
     
     string removeDigit(string number, char digit) {
-        int count = 0;
+        
         string res="0";
         string rem;
-    for (int i = 0; i < number.size(); i++)
-    {
-        if (number[i] ==  digit)
-        {
-            ++ count;
-        }
-    }
-        for(int i=0;i<number.size()&&count>0;i++){
+
+        for(int i=0;i<number.size();i++){
             rem=number;
             if(number[i]==digit){
                 
                 rem.erase(i,1);
                 
-                res=max1(res,rem);
-                count--;
+                res=max(res,rem);
+              
             }
         }
         
