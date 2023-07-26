1971a52e9eb78c06395bc52019b71b8bbb61ff93
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-07-26 22:52:47 +0530

    Update 430. Flatten a Multilevel Doubly Linked List.cpp

--- 430. Flatten a Multilevel Doubly Linked List.cpp
+++ 430. Flatten a Multilevel Doubly Linked List.cpp
@@ -1,1 +1,37 @@
 
+int count=0;
+class Solution {
+public:
+    
+    Node* flatten(Node* head) {
+        if(head==NULL)
+        return head;
+        Node* curr=head;
+        Node* curr1=NULL;
+         Node* curr2;
+         Node* curr3;
+         int count;
+        while(curr!=NULL){
+            if(curr->child!=NULL){
+                count++;
+                curr1=curr->next;
+                curr->next=curr->child;
+                curr->child=NULL;
+                curr->next->prev=curr;
+                curr3=flatten(curr->next); 
+                count--;
+                curr3->next=curr1;
+                if(curr1!=NULL)
+                curr1->prev=curr3;
+            }
+             curr2=curr;
+             curr=curr->next;
+            }
+            if(count==0) {
+            return head;
+            }
+            else
+            return curr2;
+     
+    }
+};
