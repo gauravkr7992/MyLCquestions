0cab68d67976371aa6764c00f2bd94f7693abbe7
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-07-28 21:48:49 +0530

    Update 622. Design Circular Queue.cpp

--- 622. Design Circular Queue.cpp
+++ 622. Design Circular Queue.cpp
@@ -1,1 +1,47 @@
 
+class MyCircularQueue {
+    int *q;
+    int front=-1,rear=-1;
+    int size=0;
+    int capacity;
+public:
+    MyCircularQueue(int k) {
+        q=new int[k];
+        capacity=k;
+    }
+    
+    bool enQueue(int value) {
+        if(size==capacity) return false;
+        if(front==-1) front=rear=0;
+        else rear=(rear+1)%capacity;
+        q[rear]=value;
+        size++;
+        return true;
+        
+    }
+    
+    bool deQueue() {
+        if(size==0) return false;
+        front=(front+1)%capacity;
+        size--;
+        return true;
+    }
+    
+    int Front() {
+        if(size==0) return -1;
+        return q[front];
+    }
+    
+    int Rear() {
+        if(size==0) return -1;
+        return q[rear];
+    }
+    
+    bool isEmpty() {
+        return (size==0);
+    }
+    
+    bool isFull() {
+        return (size==capacity);
+    }
+};
