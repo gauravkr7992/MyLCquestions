bbc76ef3fc62906ab953a27d9f7132b785dfd69a
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-08-21 18:24:23 +0530

    Update 31.Next Permutation.cpp

--- 31.Next Permutation.cpp
+++ 31.Next Permutation.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int idx = -1,n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        for(int i=n-1;i>idx and idx!=-1;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        reverse(nums.begin()+idx+1,nums.end());
+    }
+};
