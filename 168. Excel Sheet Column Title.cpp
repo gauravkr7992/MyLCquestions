109cd8f7510317808b621865eed8673a879c0b87
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-08-25 19:54:19 +0530

    Update 168. Excel Sheet Column Title.cpp

--- 168. Excel Sheet Column Title.cpp
+++ 168. Excel Sheet Column Title.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+           while(columnNumber){
+               columnNumber--;
+               int m=(columnNumber)%26;
+               res+=(m+'A');   //res+=(char)(m+'A');
+               columnNumber/=26;
+           }
+           reverse(res.begin(),res.end());
+      return res;
+
+    }
+};
+
+
+
+// to reverse a string =>reverse(string.begin(),string.end())
