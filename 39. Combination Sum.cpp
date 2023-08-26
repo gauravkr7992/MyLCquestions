bfe0d57810c955361cba08fe1c4ff6fdf2c093bf
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-08-26 13:57:35 +0530

    Update 39. Combination Sum.cpp

--- 39. Combination Sum.cpp
+++ 39. Combination Sum.cpp
@@ -1,1 +1,37 @@
 
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+       vector<vector<int>> result;
+       vector<int>v;
+        solve(0,candidates,target,v,result);
+        return result;
+    }
+
+
+void solve(int j,vector<int>& candidates, int target,vector<int>& v,vector<vector<int>>&result){
+     if(target==0) {
+         result.push_back(v);
+         v.pop_back();
+         return;
+     }
+     if(target<0){
+         v.pop_back();
+         return;
+     }
+
+     for(int i=j;i<candidates.size();i++){
+             
+             v.push_back(candidates[i]);
+            solve(i,candidates, target-candidates[i],v,result);
+         
+        }
+        j++;
+      v.pop_back();
+      return;
+        
+}
+
+
+
+};
