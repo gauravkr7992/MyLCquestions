482b72241436d0969719bd8d5d929b0fff183de1
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-09-16 12:40:45 +0530

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int resSum=0;
+        int Max=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            resSum+=nums[i];
+            Max=max(Max,resSum);
+            if(resSum<0) resSum=0;
+
+        }
+        return Max;
+    }
+};
+
+
+
+// Kedane's Algorithm O(n)
