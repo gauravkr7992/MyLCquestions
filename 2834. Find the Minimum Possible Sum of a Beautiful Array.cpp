6564ea3b97366d31ca19e3ec36d4e97f091899ed
Gaurav Kumar <91934323+gauravkr7992@users.noreply.github.com>
2023-09-21 19:04:33 +0530

    Update 2834. Find the Minimum Possible Sum of a Beautiful Array.cpp

--- 2834. Find the Minimum Possible Sum of a Beautiful Array.cpp
+++ 2834. Find the Minimum Possible Sum of a Beautiful Array.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    long long minimumPossibleSum(int n, int target) {
+        unordered_set<int> us;
+        long long sum=0;
+        
+        for(int val=1;us.size()<n;val++){
+            if((us.find(target-val) == us.end())){
+                us.insert(val);
+                sum+=val;
+            }
+        }
+        
+        return sum;
+    }
+};
